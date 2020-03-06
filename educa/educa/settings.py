37b67da3c3398dfd0d7ded520fd953284ba08edf
RootenberG <savichevdenis244@gmail.com>
2020-03-06 09:15:07 +0200

    Add access to course

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from django.urls import reverse_lazy
+#log required
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
